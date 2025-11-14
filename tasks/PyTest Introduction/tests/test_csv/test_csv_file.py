96e7ff69700df45b29f1aed4d0659cfc3290fc24
Julia Mendoza <juliamendozav@gmail.com>
2025-11-14 16:45:38 -0600

    Task 1

--- tasks/PyTest Introduction/tests/test_csv/test_csv_file.py
+++ tasks/PyTest Introduction/tests/test_csv/test_csv_file.py
@@ -7,54 +7,77 @@ import pytest
 class TestCVSValidation:
     #Read the cvs file
     @pytest.fixture(scope="class")
-    def get_cvs_data(self):
-        cvs_file_path = os.path.join(os.path.dirname(__file__),'tasks/PyTest Introduction/src/data/data.csv')
-        return pd.read_csv(cvs_file_path)
+    def get_csv_data(self):
+        csv_file_path = os.path.join(os.path.dirname(__file__),'../../src/data/data.csv')
+        return pd.read_csv(csv_file_path)
 
     #Test 1: validate that file is not empty
-def test_file_not_empty(self, cvs_data):
-    """Validate that file is not empty"""
-    assert len(cvs_data)>0, "CVS file should not be empty - Found empty file"
+    def test_file_not_empty(self, get_csv_data):
+        """Validate that file is not empty"""
+        assert len(get_csv_data)>0, "CVS file should not be empty - Found empty file"
 
     #Test 2: Validate the schema of the file (id, name, age, email)
-def test_validate_schema(self, cvs_data):
-    """Validate the schema of the file (id, name, age, email)"""
-    expected_columns = ['id','name','age','email','is_active']
-    actual_columns = cvs_data.columns.tolist()
+    def test_validate_schema(self, get_csv_data):
+        """Validate the schema of the file (id, name, age, email)"""
+        expected_columns = ['id','name','age','email','is_active']
+        actual_columns = get_csv_data.columns.tolist()
 
-    assert actual_columns == expected_columns, (
-        f"CVS schema mismatch. Expected columns: {expected_columns},"
-        f" found columns: {actual_columns}"
-    )
+        assert actual_columns == expected_columns, (
+            f"CVS schema mismatch. Expected columns: {expected_columns},"
+            f" found columns: {actual_columns}"
+        )
 
     #Test 3: Validate that the age column contains valid values (0-100) - Use Custom and Predefined Marks (skip)
     @pytest.mark.skip(reason="Age validation TBD")
-def test_age_column_valid(self, cvs_data):
-    """Validate that the age column contains valid values """
-    invalid_ages = cvs_data[~cvs_data['age'].between(0,100)]
-    assert len(invalid_ages) == 0,(
-        f"Invalid age values found. Age should be between 0 and 100"
-        f"Row(s)s with invalid age:{invalid_ages[['id','age']].to_dict()}")
+    def test_age_column_valid(self, get_csv_data):
+        """Validate that the age column contains valid values """
+        invalid_ages = get_csv_data[~get_csv_data['age'].between(0,100)]
+        assert len(invalid_ages) == 0,(
+            f"Invalid age values found. Age should be between 0 and 100"
+            f"Row(s)s with invalid age:{invalid_ages[['id','age']].to_dict()}")
 
     #Test 4: Validate that the email column contains valid email addressess
-def test_email_column_valid(self,cvs_data):
-    """Validate that the email column contains valid email adresses format """
-    email_pattern =  r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    invalid_emails = cvs_data[~cvs_data['email'].str.match(email_pattern, na=False)]
-    assert len(invalid_emails) == 0, (
-        f"Invalid emails values found. Email should follow standard format"
-        f"Row(s)s with invalid email:{invalid_emails[['id', 'email']].to_dict()}"
-    )
+    def test_email_column_valid(self, get_csv_data):
+        """Validate that the email column contains valid email adresses format """
+        email_pattern =  r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        invalid_emails = get_csv_data[~get_csv_data['email'].str.match(email_pattern, na=False)]
+        assert len(invalid_emails) == 0, (
+            f"Invalid emails values found. Email should follow standard format"
+            f"Row(s)s with invalid email:{invalid_emails[['id', 'email']].to_dict()}"
+        )
 
+    #Test 5: Validate there are not duplicates - Use Custom and Predefined Marks (xfail)
+    @pytest.mark.xfail(reason="Duplicate rows expected in current test data ")
+    def test_duplicates(self, get_csv_data):
+        """Validate there are not duplicate rows"""
+        duplicates = get_csv_data[get_csv_data.duplicated()]
+        assert len(duplicates) == 0, (
+            f"Duplicate rows found. Expected no duplicates, but found {len(duplicates)}"
+        )
 
-def test_active_players():
-    assert 1 + 1 == 2
+    #Test 6: Validate is active with parameter - Use Custom and Predefined Marks (parametrize)
+    @pytest.mark.parametrize("id,expected_is_active", [
+        (1, False),
+        (2, True)
+    ])
+    def test_active_players(self, get_csv_data, id, expected_is_active):
+        """Validate is_active = False for id = 1 and is_active = True for id= 2"""
+        row = get_csv_data[get_csv_data['id']==id]
+        assert len(row) == 1, f"Row with id {id} not found in CSV data"
 
+        actual_is_active = row['is_active'].iloc[0]
+        assert actual_is_active == expected_is_active, (
+            f"is_active should be {expected_is_active} for id {id},"
+            f" actual value found: {actual_is_active}"
+        )
 
-def test_active_player():
-    assert 1 + 1 == 2
+    #Test 7: Validate is active without parameter
+    def test_active_player(self, get_csv_data):
+        """Validate is_active = True for id =2 without parameter """
+        row = get_csv_data[get_csv_data['id']==2]
+        assert len(row) == 1, "ID 2 not found in CSV data"
 
-def test_duplicates():
-    assert 1 + 1 == 2
-
-
+        actual_is_active = row['is_active'].iloc[0]
+        assert actual_is_active == True, (
+            f"is_active should be True for ID 2, actual value found: {actual_is_active}"
+        )
