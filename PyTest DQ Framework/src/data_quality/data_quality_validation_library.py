177b79439f08f734c2e7164b563233d699cd2463
Julia Mendoza <juliamendozav@gmail.com>
2025-11-30 19:22:21 -0600

    Homework 2: Homework DQ Framework based on PyTest 1. postgreSQL conector 2. Data quality validation library

--- PyTest DQ Framework/src/data_quality/data_quality_validation_library.py
+++ PyTest DQ Framework/src/data_quality/data_quality_validation_library.py
@@ -11,25 +11,35 @@ class DataQualityLibrary:
     """
 
     @staticmethod
+    #Find duplicates
     def check_duplicates(df, column_names=None):
         if column_names:
-            df.duplicates(column_names)
+            duplicates = df.duplicated(subset=column_names, keep=False) #Marks ALL duplicates as True
         else:
-            df.duplicates(all_columns)
+            duplicates = df.duplicated(keep=False)
+
+        duplicates_count = duplicates.sum()
+        assert duplicates_count == 0, f"Found {duplicates_count} duplicate records"
 
     @staticmethod
+    #Compare row counts btw dataframes
     def check_count(df1, df2):
-        df1.count = df2.count
+        count1 = len(df1)
+        count2 = len(df2)
+        assert df1.count == df2.count, f"fCount mismatch: {count1} vs {count2}"
 
     @staticmethod
+    #Check if the dfs have same data
     def check_data_full_data_set(df1, df2):
         df1 = df2
 
     @staticmethod
+    #Check if the df has data
     def check_dataset_is_not_empty(df):
         df.is_not_empty
 
     @staticmethod
+    #Check not null in the columns 
     def check_not_null_values(df, column_names=None):
         col for df.column_names:
             col.not_null
